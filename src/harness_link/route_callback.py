702d5c62fcb493ef0cbbaf047d0f7f3d56c43545
sileod <damien.sileo@gmail.com>
2026-09-14 10:22:13 +0200

    feat: report LiteLLM deployment routes

--- src/harness_link/route_callback.py
+++ src/harness_link/route_callback.py
@@ -1,9 +1,13 @@
 from litellm.integrations.custom_logger import CustomLogger
 
 
+ROUTE_PREFIX = "HARNESS_LINK_ROUTE "
+
+
 class HarnessLinkRouteLogger(CustomLogger):
     async def async_post_call_success_deployment_hook(self, request_data, response, call_type):
-        return None
+        route = str(request_data.get("model") or "unknown")
+        print(ROUTE_PREFIX + route, flush=True)
 
 
 proxy_handler_instance = HarnessLinkRouteLogger()
